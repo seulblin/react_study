a566453025f052fd221963c63cb26e4a859f70ba
seulblin <subinge94@gmail.com>
2021-01-23 00:10:16 +0900

    2021.01.22 공부자료

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import Hello from './hello';
-import './App.css';
+import Counter from './Counter';
+//import Hello from './hello';
+//import Wrapper from './Wrapper';
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black', 
-    color:'aqua', 
-    fontSize: 24,
-    padding:'1rem'
-  }
+ 
   return (
-    <>
-      <div>{name}</div>
-      <div style = {style}><Hello /></div>
-      <div className="gray-box"></div>
-    </> 
+  //  <Wrapper>
+  //    <Hello name = "react" color = "red" isSpecial={true} />
+  //    <Hello color = "pink" />
+  //  </Wrapper>
+  <Counter />
   );
 }
 
--- src/hello.js
+++ src/hello.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-function Hello() {
-  return <div>안녕하세요</div>; //세미콜론은 있어도 없어도 차이가 없다.
+function Hello({color, name, isSpecial}) {
+  return (
+    <div style={{
+      color
+    }}>
+      {isSpecial && <b>*</b>}
+      안녕하세요 {name}
+    </div> //세미콜론은 있어도 없어도 차이가 없다.
+  )
 }
+
+
+Hello.defaultProps = {
+  name:'이름없음'
+};
 
 export default Hello;
