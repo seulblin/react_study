14ff01e737a7ec3a3b8e44f28044ab77af83da0f
seulblin <subinge94@gmail.com>
2021-01-25 22:40:59 +0900

    useMemo 성능최적화

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, {useRef, useState} from 'react';
+import React, {useRef, useState, useMemo} from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중..')
+  return users.filter(user => user.active).length;
+}
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -72,6 +77,7 @@ function App() {
     ));
   };
 
+  const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
     <>
@@ -82,6 +88,8 @@ function App() {
       onCreate={onCreate}
      />
     <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+    <div>활성 사용자 수 : {count}</div>
+   
     </>
   );
 }
