6379be3b09b5c206b57836051efc0517d4ad447e
seulblin <subinge94@gmail.com>
2021-01-25 22:15:45 +0900

    배열에 있는 항목 삭제하는 법

--- src/App.js
+++ src/App.js
@@ -55,7 +55,11 @@ function App() {
 
     console.log(nextId.current); ///nextId의 값을 호출하고 싶을 때
     nextId.current += 1; // 값이 바뀌고 싶을 때.
-  }
+  };
+
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id));
+  };
 
   return (
     <>
@@ -65,7 +69,7 @@ function App() {
       onChange={onChange}
       onCreate={onCreate}
      />
-    <UserList users={users} />
+    <UserList users={users} onRemove={onRemove} />
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,19 +1,29 @@
 import React from 'react';
 
-function User({ user }) {
+
+//삭제 버튼이 눌릴 때,  onRemove가 동작하기!
+function User({ user, onRemove }) {
+  const { username, email, id} = user;
   return(
     <div>
-        <b>{user.username}</b> <span>({user.email})</span>
+        <b>{username}</b> <span>({email})</span>
+        <button onClick={() => onRemove(id)}>삭제</button>
     </div>
   );
 } 
 
-function UserList({ users }){
+function UserList({ users, onRemove }){
     return(
     <div>
       {
         users.map(
-          user => (<User user={user} key={user.id} />)
+            user => (
+            <User 
+              user={user} 
+              key={user.id} 
+              onRemove={onRemove}
+            />
+          )
         )
       }
     </div>
